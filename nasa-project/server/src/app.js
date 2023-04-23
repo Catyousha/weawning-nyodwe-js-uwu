afb85a93727494d0e3823665931829ef79056021
Catyousha <jaka-asa@zero-one-group.com>
2023-04-23 15:43:02 +0700

    chore(nasa-project): set combined logging for morgan

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(planetsRouter);
 
