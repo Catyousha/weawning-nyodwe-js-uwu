0a6daac9d9a9a4d5ad99f8480a5a5bf1a3b23712
Catyousha <jaka-asa@zero-one-group.com>
2023-04-29 21:09:43 +0700

    feat(nasa-project): initiate mongo schema

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,4 +1,6 @@
+const dotenv = require('dotenv').config();
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
@@ -6,7 +8,15 @@ const { loadPlanetsData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection has been established.');
+})
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+})
+
 async function startServer() {
+    await mongoose.connect(process.env.MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
